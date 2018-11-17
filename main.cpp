7ef13287b40c66174b689c66753f40b9a105bfde
Henrique Penna <henrique.p.diniz@ufv.br>
2018-11-16 22:53:46 -0200

    SUBMENU COMPLETISSIMO

--- main.cpp
+++ main.cpp
@@ -33,24 +33,33 @@ public:
         scaleX = 3.1; scaleY = 3.2; scaleZ = 3.3;
         rotX = 2.1; rotY = 2.2; rotZ = 2.3; rotAngle = 0.5;
     }
+    void imprime(){
+        cout<<"X: "<<transX<<" Y: "<<transY<<" Z: "<<transZ<<endl;
+        cout<<"Angle: "<<rotAngle<<" X: "<<rotX<<" Y: "<<rotY<<" Z: "<<rotZ<<endl;
+        cout<<"X: "<<scaleX<<" Y: "<<scaleY<<" Z: "<<scaleZ<<endl;
+        cout<<endl;
+    }
+
 };
 
 int objs = 0;
 string nomeArq;
 objeto obj[3];
 bool importado[3];
+bool clicked[3][3][4]; // 0 PARA FALSO, 1 PARA TRUE
+string novoP = "";
 unsigned short int arq = 0;
 unsigned short int selecionado = 0; // 0 PARA O OBJETO 3D, 1 PARA BARRA DE PESQUISA
 bool enviar = false;
 bool nomeValido = false;
 vector<vector<GLfloat> > vertices;
-vector<vector<GLfloat> > vertices1;
 vector<vector<GLfloat> > vertices2;
 vector<vector<GLfloat> > vertices3;
 vector<vector<int> > faces;
 vector<vector<int> > faces2;
 vector<vector<int> > faces3;
 const int font = (int)GLUT_BITMAP_TIMES_ROMAN_24;
+const int font2 = (int)GLUT_BITMAP_HELVETICA_18;
 
 GLdouble viewer[] = {20.0, 20.0, 30.0};
 
@@ -72,7 +81,7 @@ void submenu(int x_ini, int y_ini, int index){
     int x = x_ini, y = y_ini;
     int x_inc = 15, x_spacer = 20;
     int y_inc = 28, y_spacer = 55;
-
+    //obj[arq-1].imprime();
     stringstream ss; //from
     ss << obj[index].nome;
     string s = ss.str ();
@@ -84,20 +93,19 @@ void submenu(int x_ini, int y_ini, int index){
     for(int j=0; j<=2;j++){
 
         glColor3f(0.0, 0.0, 0.0);
+       // cout<< setprecision(1) << obj[index].transX << fixed <<endl;
         stringstream ss;
 
-       // cout<< setprecision(1) << obj[index].transX << fixed <<endl;
+            if(j == 0)
+                ss <<obj[index].transX;
+            else if(j == 1)
+                ss << obj[index].transY;
+            else if(j == 2)
+                ss << obj[index].transZ;
 
-        if(j == 0)
-            ss <<obj[index].transX;
-        else if(j == 1)
-            ss << obj[index].transY;
-        else if(j == 2)
-            ss << obj[index].transZ;
-
-        std::string s = ss.str ();
-        char* char_type = (char*) s.c_str();
-        renderBitmapString(x+2,y+y_inc-4,(void *)font,char_type);
+            std::string s = ss.str ();
+            char* char_type = (char*) s.c_str();
+            renderBitmapString(x+1,y+y_inc-6,(void *)font2,char_type);
 
         glColor3f(1.0, 1.0, 1.0);
         glBegin(GL_POLYGON);
@@ -122,18 +130,18 @@ void submenu(int x_ini, int y_ini, int index){
         glColor3f(0.0, 0.0, 0.0);
         stringstream ss;
 
-        if(j == 0)
-            ss << obj[index].rotAngle;
-        else if(j == 1)
-            ss << obj[index].rotX;
-        else if(j == 2)
-            ss << obj[index].rotY;
-        else if(j == 3)
-            ss << obj[index].rotZ;
+            if(j == 0)
+                ss << obj[index].rotAngle;
+            else if(j == 1)
+                ss << obj[index].rotX;
+            else if(j == 2)
+                ss << obj[index].rotY;
+            else if(j == 3)
+                ss << obj[index].rotZ;
 
-        std::string s = ss.str ();
-        char* char_type = (char*) s.c_str();
-        renderBitmapString(x+2,y+y_inc-4,(void *)font,char_type);
+            std::string s = ss.str ();
+            char* char_type = (char*) s.c_str();
+            renderBitmapString(x+1,y+y_inc-6,(void *)font2,char_type);
 
         glColor3f(1.0, 1.0, 1.0);
         glBegin(GL_POLYGON);
@@ -152,20 +160,19 @@ void submenu(int x_ini, int y_ini, int index){
     glColor3f(1.0, 1.0, 1.0);
     // Desenha inputs escala
     for(int j=0; j<=2;j++){
-
         glColor3f(0.0, 0.0, 0.0);
         stringstream ss;
 
-        if(j == 0)
-            ss << obj[index].scaleX;
-        else if(j == 1)
-            ss << obj[index].scaleY;
-        else if(j == 2)
-            ss << obj[index].scaleZ;
+            if(j == 0)
+                ss << obj[index].scaleX;
+            else if(j == 1)
+                ss << obj[index].scaleY;
+            else if(j == 2)
+                ss << obj[index].scaleZ;
 
-        std::string s = ss.str ();
-        char* char_type = (char*) s.c_str();
-        renderBitmapString(x+2,y+y_inc-4,(void *)font,char_type);
+            std::string s = ss.str ();
+            char* char_type = (char*) s.c_str();
+            renderBitmapString(x+1,y+y_inc-6,(void *)font2,char_type);
 
         glColor3f(1.0, 1.0, 1.0);
 
@@ -181,6 +188,46 @@ void submenu(int x_ini, int y_ini, int index){
 
 
 }
+
+void mudaParam(int j, int i, int ob, float valor){
+
+                    if( j == 0 ){ // TRANSLACAO
+                        if( i == 0 ){
+                            obj[ob].transX = valor;
+                        }
+                        if( i == 1 ){
+                            obj[ob].transY = valor;
+                        }
+                        if( i == 2 ){
+                            obj[ob].transZ = valor;
+                        }
+                    }
+                    else if( j == 1 ){ // ROTACAO
+                        if( i == 0 ){
+                            obj[ob].rotAngle = valor;
+                        }
+                        if( i == 1 ){
+                            obj[ob].rotX = valor;
+                        }
+                        if( i == 2 ){
+                            obj[ob].rotY = valor;
+                        }
+                        if( i == 3 ){
+                            obj[ob].rotZ = valor;
+                        }
+                        }
+                    else if( j == 2 ){ // ESCALA
+                        if( i == 0 ){
+                            obj[ob].scaleX =  valor;
+                        }
+                        if( i == 1 ){
+                            obj[ob].scaleY =  valor;
+                        }
+                        if( i == 2 ){
+                            obj[ob].scaleZ = valor;
+                        }
+                    }
+                }
 
 void desenhaEixos() {}
 
@@ -226,59 +273,28 @@ void desenhaMenuLateral(){
 }
 
 void tri(int a, int b, int c) {
-    cout << vertices.size() << endl;
-    cout << vertices[a][0] << ' ' << vertices[a][1] << ' ' << vertices[a][2] << endl;
-    if(a < vertices.size() && b < vertices.size() && c < vertices.size()){
-        glColor3f(1.0,0.0,0.0);
-        glBegin(GL_TRIANGLES);
-            glVertex3f(vertices[a][0],vertices[a][1],vertices[a][2]);
-            glVertex3f(vertices[b][0],vertices[b][1],vertices[b][2]);
-            glVertex3f(vertices[c][0],vertices[c][1],vertices[c][2]);
-        glEnd();
-    }
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(vertices[a][0],vertices[a][1],vertices[a][2]);
+        glVertex3f(vertices[b][0],vertices[b][1],vertices[b][2]);
+        glVertex3f(vertices[c][0],vertices[c][1],vertices[c][2]);
+    glEnd();
 }
-
-void tri2(int a, int b, int c) {
-    cout << vertices2.size() << endl;
-    cout << vertices2[a][0] << ' ' << vertices2[a][1] << ' ' << vertices2[a][2] << endl;
-    if(a < vertices2.size() && b < vertices2.size() && c < vertices2.size()){
-        glColor3f(1.0,0.0,0.0);
-        glBegin(GL_TRIANGLES);
-            glVertex3f(vertices2[a][0],vertices2[a][1],vertices2[a][2]);
-            glVertex3f(vertices2[b][0],vertices2[b][1],vertices2[b][2]);
-            glVertex3f(vertices2[c][0],vertices2[c][1],vertices2[c][2]);
-        glEnd();
-    }
-}
-
-void tri3(int a, int b, int c) {
-    cout << vertices3.size() << endl;
-    cout << vertices3[a][0] << ' ' << vertices3[a][1] << ' ' << vertices3[a][2] << endl;
-    if(a < vertices3.size() && b < vertices3.size() && c < vertices3.size()){
-        glColor3f(1.0,0.0,0.0);
-        glBegin(GL_TRIANGLES);
-            glVertex3f(vertices3[a][0],vertices3[a][1],vertices3[a][2]);
-            glVertex3f(vertices3[b][0],vertices3[b][1],vertices3[b][2]);
-            glVertex3f(vertices3[c][0],vertices3[c][1],vertices3[c][2]);
-        glEnd();
-    }
-}
-
 
 void desenhaObj(int x) {
     if(x == 0){
-        for(int i=0; i< faces.size()-1; i++){
-            tri(faces[i][0], faces[i][1], faces[i][2]);
+        for(int i=0; i< vertices.size()-2; i++){
+            tri(i,i+1,i+2);
         }
     }
     else if(x == 1){
-        for(int i=0; i< faces2.size(); i++){
-            tri2(faces2[i][0], faces2[i][1], faces2[i][2]);
+        for(int i=0; i< vertices2.size()-2; i++){
+            tri(i,i+1,i+2);
         }
     }
     else if(x == 2){
-        for(int i=0; i< faces3.size(); i++){
-            tri3(faces3[i][0], faces3[i][1], faces3[i][2]);
+        for(int i=0; i< vertices3.size()-2; i++){
+            tri(i,i+1,i+2);
         }
     }
 }
@@ -314,7 +330,6 @@ void leObj(string nome){
     face.push_back(0);
     face.push_back(0);
     int v1, v2, v3, ignora;
-    string s1, s2, s3;
     char barra;
 
 	while(!arquivo.eof()){
@@ -339,36 +354,38 @@ void leObj(string nome){
 
 		}
 		else if(tipo == "f"){
-            arquivo >> s1;
-            arquivo >> s2;
-            arquivo >> s3;
-
-            std::string::size_type sz;   // alias of size_t
-
-            int pos = s1.find('/');
-            v1 = std::stoi( s1.substr(0, pos), &sz, 10 );
-            pos = s2.find('/');
-            v2 = std::stoi( s2.substr(0, pos), &sz, 10 );
-            pos = s3.find('/');
-            v3 = std::stoi( s3.substr(0, pos), &sz, 10 );
-
-            if(v1 < 0) v1 = v1 * (-1);
-            if(v2 < 0) v2 = v2 * (-1);
-            if(v3 < 0) v3 = v3 * (-1);
+            arquivo>>v1;
+           // cout << "vertice: " << v1;
+            arquivo>>barra;
+            arquivo>>ignora;
+            arquivo>>barra;
+            arquivo>>ignora;
+            arquivo>>v2;
+            //cout << "vertice: " << v2;
+            arquivo>>barra;
+            arquivo>>ignora;
+            arquivo>>barra;
+            arquivo>>ignora;
+            arquivo>>v3;
+           // cout << "vertice: " << v3;
+            arquivo>>barra;
+            arquivo>>ignora;
+            arquivo>>barra;
+            arquivo>>ignora;
 
             face[0] = v1;
             face[1] = v2;
             face[2] = v3;
 
             //cout << "face: " << face[0] << ' ' << face[1] << ' ' << face[2] << endl;
-
+    /*
             if(arq == 0)
                 faces.push_back(face);
             else if(arq == 1)
                 faces2.push_back(face);
             else if(arq == 2)
                 faces3.push_back(face);
-
+*/
 		}
 
 		//cout<<aux<<endl;
@@ -380,54 +397,27 @@ void mouseHandler(int button, int state, int x, int y){
     if (button == GLUT_LEFT_BUTTON){
         if (state == GLUT_DOWN) {
             if( x > LARGURA_JANELA - LARGURA_JANELA/3){
-               // cout<< "Cliquei em " << x << " " << y << endl;
                 if(x> 615 && x<795 && y>BAIXO_IMPORTAR && y<CIMA_IMPORTAR)
                     selecionado = 1;
                 //unsigned short int limite = 0;
-                for(int i=0; i < 4; i++){
-                    for(int j=0; j < 3; j++){
-                        if(i == 3 && j!= 1){
-                            continue;
-                        }
-                        if(x> 615+(i*60) && x<660+(i*60) && y>105+(j*55) && y<133+(j*55)){
-                            if( j == 0 ){ // TRANSLACAO
-                                if( i == 0 ){
-                                    obj[0].transX = x;
-                                }
-                                if( i == 1 ){
-                                    obj[0].transY = x;
-                                }
-                                if( i == 2 ){
-                                    obj[0].transZ = x;
-                                }
+                for(int k=0; k<3; k++){
+                    for(int i=0; i < 4; i++){
+                        for(int j=0; j < 3; j++){
+                            if(i == 3 && j!= 1){
+                                continue;
                             }
-                            if( j == 1 ){ // ROTACAO
-                                if( i == 0 ){
-                                    obj[0].rotAngle = x;
+                            if(x> 615+(i*60) && x<660+(i*60) && y>105+(j*55)+(k*210) && y<133+(j*55)+(k*210)){
+                                selecionado = 2;
+                                novoP = "";
+                                for(int k2=0; k2<3; k2++){
+                                    for (int i2=0; i2<4; i2++){
+                                        for(int j2=0; j2<3; j2++){
+                                            clicked[k2][j2][i2] = 0;
+                                        }
+                                    }
                                 }
-                                if( i == 1 ){
-                                    obj[0].rotX = x;
-                                }
-                                if( i == 2 ){
-                                    obj[0].rotY = x;
-                                }
-                                if( i == 3 ){
-                                    obj[0].rotZ = x;
-                                }
+                                clicked[k][j][i] = 1;
                             }
-                            if( j == 2 ){ // ESCALA
-                                if( i == 0 ){
-                                    obj[0].scaleX = x;
-                                }
-                                if( i == 1 ){
-                                    obj[0].scaleY = x;
-                                }
-                                if( i == 2 ){
-                                    obj[0].scaleZ = x;
-                                }
-                            }
-                            cout<< " linha "<< j<< endl;
-                            cout<< " coluna "<< i<< endl;
                         }
                     }
                 }
@@ -470,6 +460,7 @@ void display(){
     glViewport((LARGURA_JANELA/3)*2, 0, LARGURA_JANELA, ALTURA_JANELA);
     gluOrtho2D(0, LARGURA_JANELA/3, ALTURA_JANELA, 0);
     glMatrixMode(GL_MODELVIEW);
+
     desenhaMenuLateral();
 
     glutSwapBuffers();
@@ -488,13 +479,7 @@ void leVertices(){
 void keyboardHandler(unsigned char key, int x, int y){
     if (key == 27) exit(0); //ESC
 
-    cout<< "ASCII de "<<key<< ": "<<(int)key << endl;
-
-    if(arq>0){
-        for(int i=0; i<arq; i++)
-            cout<<i<<": "<< obj[i].nome << " ";
-        cout<<endl;
-    }
+   // cout<< "ASCII de "<<key<< ": "<<(int)key << endl;
 
     if(selecionado == 1){
         if(arq < 3){ //IMPEDIR USUARIO MANDAR MAIS DE 3 ARQUIVOS
@@ -505,6 +490,45 @@ void keyboardHandler(unsigned char key, int x, int y){
             }
             else if( (int)key == 8 && obj[arq].nome.size()>0){ // CODIGO ASCII 8 DO BOTÃO BACKSPACE
                 obj[arq].nome.erase(obj[arq].nome.size()-1); // APAGA ULTIMO CARACTERE DA STRIING
+            }
+            else if((int)key == 13){
+                leObj(obj[arq].nome);
+                obj[arq].nome = obj[arq].nome;
+                importado[arq] = true;
+                arq ++;
+                selecionado = 0;
+            }
+        }
+    }
+    else if(selecionado == 2){
+        for(int k=0; k<3; k++){
+            for (int i=0; i<4; i++){
+                for(int j=0; j<3; j++){
+                    if(clicked[k][j][i] == 1){
+                        if((int)key == 46 || ((int)key >= 48 && (int)key <= 57 ) || (int)key == 13 || (int)key == 8){ //Codigo ASCII apenas de numeros e ponto
+                            if( (int)key == 8){ // CODIGO ASCII 8 DO BOTÃO BACKSPACE
+                                if(novoP.size()>0){
+                                    novoP.erase(novoP.size()-1);
+                                    if(novoP.size() == 0)
+                                        mudaParam(j,i,k,0);
+                                    else
+                                        mudaParam(j,i,k,stof(novoP));
+                                }
+                            }
+                            else {
+                                string aux;
+                                aux = key;
+                                novoP = novoP + aux;
+                                mudaParam(j,i,k,stof(novoP));
+                                if((int)key == 13){
+                                    mudaParam(j,i,k,stof(novoP));
+                                    clicked[k][j][i] == 0;
+                                    novoP = "";
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -524,8 +548,13 @@ void reshape(int x, int y) {
 }
 
 void init(){
-    for(int i=0; i<3; i++)
-        importado[i] = false;
+    for(int k=0; k<3; k++){
+        importado[k] = false;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++)
+                clicked[k][i][j] = 0;
+        }
+    }
     glClearColor(1.0, 1.0, 1.0, 1.0);
     //leVertices();
 }
